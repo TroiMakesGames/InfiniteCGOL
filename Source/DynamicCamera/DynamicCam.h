d94e5748b7cdaa827c095917a6acd8230ab8d8bc
TroiMakesGames <zapis.troi@gmail.com>
2026-08-25 16:19:24 +0200

    Created an InputField class as a .h module

---
+++ Source/DynamicCamera/DynamicCam.h
@@ -0,0 +1,93 @@
+#pragma once    //use script in compiled build only once
+
+#include "raylib.h"
+#include "raymath.h"
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+class Viewport 
+{
+    public:
+        Vector2 worldPos;
+        float zoom;
+
+    private:
+        float movementSpeed;
+        float currMovementSpeed;
+
+        float zoomSpeed;
+        float minZoom;
+        float maxZoom;
+
+    public:
+    Viewport(Vector2 worldPos, float movementSpeed, float zoomSpeed, float minZoom, float maxZoom) 
+    {
+        this->worldPos = worldPos;
+        this->movementSpeed = movementSpeed;
+        this->currMovementSpeed = movementSpeed;
+
+        this->zoom = 1.0f;
+        this->zoomSpeed = zoomSpeed;
+        this->minZoom = minZoom;
+        this->maxZoom = maxZoom;
+    }
+
+    void move(float deltaTime) 
+    {
+        //get key inputs and move camera world pos
+        Vector2 direction = Vector2 (0, 0);
+        bool keyPressed = false;
+
+        if (IsKeyDown(KEY_W)) 
+        {direction.y -= 1; keyPressed = true;}
+        if (IsKeyDown(KEY_S))
+        {direction.y += 1; keyPressed = true;}
+        if (IsKeyDown(KEY_A))
+        {direction.x -= 1; keyPressed = true;}
+        if (IsKeyDown(KEY_D))
+        {direction.x += 1; keyPressed = true;}
+
+        //normalize direction for proper diagonal scale, multiply with movement speed and apply
+        if (keyPressed)
+        {
+            direction = Vector2Normalize(direction);
+            worldPos = Vector2Add(worldPos, Vector2Scale(direction, currMovementSpeed * deltaTime * 60));
+        }
+    }
+
+    void zoomCamera()
+    {
+        //get mouse wheel inputs, scale with zoomSpeed and clamp to min/max zoom
+        float wheel = GetMouseWheelMove();
+        zoom += wheel * zoomSpeed;
+        zoom = Clamp(zoom, minZoom, maxZoom);
+
+        //also scale movement speed
+        currMovementSpeed = movementSpeed / zoom;
+    }
+
+    Vector2 worldToScreenPos(Vector2 objectWorldPos)
+    {
+        //get screen center to adjust for zoom
+        Vector2 originToScreenCenter = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
+        
+        Vector2 objToView = Vector2Subtract(objectWorldPos, worldPos);
+        objToView = Vector2Scale(objToView, zoom);
+
+        return Vector2Add(originToScreenCenter, objToView);
+    }
+
+    Vector2 screenToWorldPos(Vector2 screenPos)
+    {
+        Vector2 screenCenter = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
+
+        Vector2 screenToCenter = Vector2Subtract(screenPos, screenCenter);
+        screenToCenter = Vector2Scale(screenToCenter, 1.0f / zoom);
+        
+        return Vector2Add(screenToCenter, worldPos);
+    }
+
+    Vector2 worldToScreenScale(Vector2 objectWorldScale)
+    {return Vector2Scale(objectWorldScale, zoom);}
+};
---
+++ Source/RaylibUI/InputField.cpp
@@ -0,0 +1,212 @@
+#include "raylib.h"
+#include "raymath.h"
+#include <string>
+#include <iostream>
+#include <cmath>
+
+class InputField
+{
+    public:
+        int width;
+        int height;
+        int borderWidth;
+
+        std::string fillerText;
+        std::string text;
+
+        int textSize;
+
+        Color backColor;
+        Color borderColor;
+
+        Color selectedBackColor;
+        Color selectedBorderColor;
+
+        Color textColor;
+        Color fillerTextColor;
+        Color selectedFillerTextColor;
+
+    private:
+
+        bool isSelected = false;
+        int cursorPosition = 0;
+        int scrollOffset = 0;
+
+    public:
+
+    InputField(int width, int height, int borderWidth, std::string fillerText, int textSize, Color backColor = DARKGRAY, Color borderColor = GRAY, Color selectedBackColor = GRAY, Color selectedBorderColor = LIGHTGRAY, Color textColor = WHITE, Color fillerTextColor = GRAY, Color selectedFillerTextColor = LIGHTGRAY)
+    {
+        this->width = width;
+        this->height = height;
+        this->borderWidth = borderWidth;
+
+        this->fillerText = fillerText;
+        this->textSize = textSize;
+
+        this->backColor = backColor;
+        this->borderColor = borderColor;
+
+        this->selectedBackColor = selectedBackColor;
+        this->selectedBorderColor = selectedBorderColor;
+
+        this->textColor = textColor;
+        this->fillerTextColor = fillerTextColor;
+        this->selectedFillerTextColor = selectedFillerTextColor;
+    }
+
+    void Draw(int x, int y)
+    {
+        //get rectnagle field
+        Rectangle field = {(float)x, (float)y, (float)width, (float)height};
+
+        //check for mouse sleection
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {isSelected = CheckCollisionPointRec(GetMousePosition(), field);}
+
+        //key inputs - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        if (isSelected)
+        {
+            int key = GetCharPressed();
+
+            //use while loop for quueueing new characters
+            while (key > 0)
+            {
+                //check wheter keycode is within printable characters
+                if (key >= 32 && key <= 126)
+                {
+                    //insert within cursor pos and increment cursor pos
+                    text.insert(text.begin() + cursorPosition, static_cast<char>(key));
+                    cursorPosition++;
+                }
+
+                //add any other potential characters into the queue
+                key = GetCharPressed();
+            }
+
+            //backspace and delete (remove from left/right of the cursor)
+            if (IsKeyPressed(KEY_BACKSPACE) && cursorPosition > 0)
+            {text.erase(cursorPosition - 1, 1); cursorPosition--;}
+            if (IsKeyPressed(KEY_DELETE) && cursorPosition < (int)text.length())
+            {text.erase(cursorPosition, 1);}
+
+            //left and right
+            if (IsKeyPressed(KEY_LEFT) && cursorPosition > 0)
+            {cursorPosition--;}
+            if (IsKeyPressed(KEY_RIGHT) && cursorPosition < (int)text.length())
+            {cursorPosition++;}
+        }
+
+        //text scrolling  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        //get space available
+        int textAreaWidth = width - borderWidth * 2 - 10;
+
+        //text widht left of cursor
+        std::string beforeCursor = text.substr(0, cursorPosition);
+        int cursorX = MeasureText(beforeCursor.c_str(), textSize);
+
+        //adjust crolloffset so the cursor is always visible
+        if (cursorX - scrollOffset > textAreaWidth)
+        {scrollOffset = cursorX - textAreaWidth;}
+
+        if (cursorX - scrollOffset < 0)
+        {scrollOffset = cursorX;}
+
+        //clamp to start
+        if (scrollOffset < 0)
+        {scrollOffset = 0;}
+
+        //draw elements  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        //draw backdrop and border
+        Color currentBackColor = isSelected ? selectedBackColor : backColor;
+        Color currentBorderColor = isSelected ? selectedBorderColor : borderColor;
+
+        DrawRectangleRec(field, currentBackColor);
+        DrawRectangleLinesEx(field, borderWidth, currentBorderColor);
+
+        //draw text
+        int textX = x + borderWidth + 5;
+        int textY = y + (height - textSize) / 2;
+
+        //use Scissor mode for draws to be strictly visible within a rect
+        BeginScissorMode(x + borderWidth, y + borderWidth, width - borderWidth * 2, height - borderWidth * 2);
+
+        //filler text
+        if (text.empty())
+        {
+            if (isSelected)
+            {DrawText(fillerText.c_str(), textX, textY, textSize, selectedFillerTextColor);}
+            else
+            {DrawText(fillerText.c_str(), textX, textY, textSize, fillerTextColor);}
+        }
+        //acctualy text
+        else
+        {DrawText(text.c_str(), textX - scrollOffset, textY, textSize, textColor);}
+
+        //draw blinking cursor line
+        if (isSelected)
+        {
+            //blink functionality (timing)
+            if (((int)(GetTime() * 2) % 2) == 0)
+            {
+                int cursorScreenX = textX + cursorX - scrollOffset;
+                DrawLine(cursorScreenX, y + borderWidth + 5, cursorScreenX, y + height - borderWidth - 5, textColor);
+            }
+        }
+
+        EndScissorMode();
+    }
+
+    //helper funcs
+    std::string GetText()
+    {return text;}
+
+    void Clear()
+    {
+        text.clear();
+        cursorPosition = 0;
+        scrollOffset = 0;
+    }
+
+    bool IsSelected()
+    {return isSelected;}
+};
+
+int main()
+{
+    //screen initialisation
+    const int WIDTH = 750;
+    const int HEIGHT = 450;
+    InitWindow(WIDTH, HEIGHT, "Proper input field UI element");
+    SetTargetFPS(60);
+
+    //variable initialisation
+    InputField filename_inpt(300, 50, 2, "file.txt...", 20);
+
+    //while loop
+    while (!WindowShouldClose()) 
+    {
+        //update
+        // ...
+
+        //rendering
+        BeginDrawing();
+        ClearBackground(Color{30, 30, 30, 255});
+        
+        filename_inpt.Draw(100, 100);
+        
+        if (filename_inpt.IsSelected() && IsKeyPressed(KEY_ENTER))
+        {
+            std::cout << filename_inpt.GetText() << std::endl;
+            filename_inpt.Clear();
+        }
+        
+        //draw fps
+        DrawText(TextFormat("%i", GetFPS()), 10, 14, 20, Color{0, 0, 0, 100});
+        DrawText(TextFormat("%i", GetFPS()), 10, 11, 20, Color{0, 150, 0, 255});
+        DrawText(TextFormat("%i", GetFPS()), 10, 10, 20, Color{0, 255, 0, 255});
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
